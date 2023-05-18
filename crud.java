60e5f24a809ec205833ab5f96af09b6724fe1fec
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-17 21:49:21 -0300

    Desenvolvimento de um CRUD em Java

---
+++ crud.java
@@ -0,0 +1,12 @@
+package cursoJava;
+
+public class crud {
+
+	public static void main(String[] args) {
+		// Primeiros códigos de um CRUD 
+		
+		System.out.println("Bem vindo ao Projeto CRUD");
+
+	}
+
+}
