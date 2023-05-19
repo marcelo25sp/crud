a101b290a3bc01315a8a8fe3544f11d08dcf74ce
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-18 22:32:39 -0300

    Adicionando funcionalidades

--- crud.java
+++ crud.java
@@ -1,11 +1,18 @@
 package cursoJava;
 
+import java.util.Scanner;
+
 public class crud {
 
 	public static void main(String[] args) {
 		// Primeiros códigos de um CRUD 
 		
 		System.out.println("Bem vindo ao Projeto CRUD");
+		System.out.println("Digite o nome do usuário:");
+		Scanner teclado = new Scanner(System.in);
+		String nome;
+		nome = teclado.next();
+		System.out.println("Bem vindo(a) " + nome);
 
 	}
 
