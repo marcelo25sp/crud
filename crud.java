1afb0f0d8016bfb5b1db9299c0f561fa5daa9a4c
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-19 22:31:04 -0300

    Adicionando comentários ao projeto

--- crud.java
+++ crud.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class crud {
 
 	public static void main(String[] args) {
-		// Primeiros códigos de um CRUD 
+		
 		
 		System.out.println("Bem vindo ao Projeto CRUD");
 		System.out.println("Digite o nome do usuário:");
@@ -13,6 +13,9 @@ public class crud {
 		String nome;
 		nome = teclado.next();
 		System.out.println("Bem vindo(a) " + nome);
+		
+		//Esse projeto será desenvolvido para a mentoria do Pedro Marins
+		
 
 	}
 
