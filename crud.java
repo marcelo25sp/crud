0fdd7efc5ec838358aafabce33d9d8d558602739
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-21 19:02:50 -0300

    Adicionando as opções do usuário

--- crud.java
+++ crud.java
@@ -1,4 +1,4 @@
-package cursoJava;
+package javaCurso;
 
 import java.util.Scanner;
 
@@ -12,11 +12,30 @@ public class Crud {
 		Scanner teclado = new Scanner(System.in);
 		String nome;
 		nome = teclado.next();
-		System.out.println("Bem vindo(a) " + nome);
+		System.out.println("Bem vindo(a)!! " + nome);
 		
-		//Esse projeto será desenvolvido para a mentoria do Pedro Marins
+		System.out.println("---------------------------");
+		System.out.println("DIGITE A OPERAÇÃO DESEJADA:");
+		System.out.println("---------------------------");
+		System.out.println("1- Registar!!");
+		System.out.println("2- Listar");
+		System.out.println("3- Atualizar");
+		System.out.println("4- Deletar");
+		System.out.println("5- Remuneração Total");
+		System.out.println("6- Sair");
+		
+		Scanner sc = new Scanner(System.in);
+		int opcao;
+		opcao = sc.nextInt();
+		
+		
+		
+		
+		
+		
 		
 
+		
 	}
 
 }
